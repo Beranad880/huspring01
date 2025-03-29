70d72190ad5ac9653244b34ec4d122d339223a33
Adam Pokorný <82777487+Beranad880@users.noreply.github.com>
2025-03-29 21:20:39 +0100

    init

---
+++ src/main/java/com/example/humbleshowcase/HumbleshowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.humbleshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumbleshowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumbleshowcaseApplication.class, args);
+    }
+
+}
